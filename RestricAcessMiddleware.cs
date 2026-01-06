c62b76d8871c578bf5ed44f716bc9fe5b750c2a9
Laurent2526 <thanhmaido24@gmail.com>
2026-01-07 01:06:47 +0700

    Add files via upload

---
+++ RestricAcessMiddleware.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace shared
+{
+    public class RestricAcessMiddleware(RequestDelegate next)
+    {
+        public async Task InvokeAsync(HttpContext context)
+        {
+            var referer = context.Request.Headers["referrer"].FirstOrDefault();
+            if (string.IsNullOrEmpty(referer))
+            {
+                context.Response.StatusCode = StatusCodes.Status403Forbidden;
+                await context.Response.WriteAsync("Hmm, can't reach this page");
+                return;
+            }
+            else
+            {
+                await next(context);
+            }
+
+        }
+    }
+}
